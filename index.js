2af71e2b1d87bc8219edfadc35c8d19e6bb040e2
pcJitsu <antonios.zikos1@gmail.com>
2023-08-05 16:09:37 -0500

    Finished questions section

--- index.js
+++ index.js
@@ -11,14 +11,34 @@ const questions = [
     name: "title",
   },
   {
-    type: "password",
-    message: "What is your password?",
-    name: "password",
+    type: "input",
+    message: "What is the necessary license type?",
+    name: "license",
   },
   {
-    type: "password",
-    message: "Re-enter password to confirm:",
-    name: "confirm",
+    type: "input",
+    message: "Add a Description to your Project:",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Describe how to use this App",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Provide installation steps for this application",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Provide contribution information here",
+    name: "contributions",
+  },
+  {
+    type: "input",
+    message: "explain how to test this app",
+    name: "tests",
   },
 ];
 
@@ -40,4 +60,18 @@ function init() {
 // Function call to initialize app
 init();
 
-//Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
