b6f21fa2a6668dbf57d77cdbad681406c0d17a3d
pcJitsu <antonios.zikos1@gmail.com>
2023-08-05 18:37:15 -0500

    Finished License section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -24,7 +24,17 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return "";
+  }
+  return `
+## License
+
+This project is licensed under the ${license} License.  
+For more information, please visit ${renderLicenseLink(license)}.
+`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -57,7 +67,7 @@ ${data.contributions}
 ## Tests
 ${data.tests}
 
-## License
+
 ${renderLicenseSection(data.license)}
 
 ## Questions
